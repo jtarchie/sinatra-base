3571d5b695256fd74baf66f188d3f0cc5e364add
JT Archie <jtarchie@gmail.com>
2012-01-28 14:23:06 -0500

    database cleaner for rspec

---
+++ spec/support/database.rb
@@ -0,0 +1,18 @@
+require 'database_cleaner'
+require 'database_cleaner/cucumber'
+RSpec.configure do |config|
+
+  config.before(:suite) do
+    DatabaseCleaner.strategy = :transaction
+    DatabaseCleaner.clean_with(:truncation)
+  end
+
+  config.before(:each) do
+    DatabaseCleaner.start
+  end
+
+  config.after(:each) do
+    DatabaseCleaner.clean
+  end
+
+end
