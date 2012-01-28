a308f3696a66f3bbde69cd1b3a6034672a9c438a
JT Archie <jtarchie@gmail.com>
2012-01-28 18:48:20 -0500

    load support files from right directory

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,7 +3,7 @@ require File.expand_path(File.dirname(__FILE__) + "/../config/boot")
 
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
-Dir[File.join(File.dirname(__FILE__), "spec/support/**/*.rb")].each {|f| require f}
+Dir[File.join(File.dirname(__FILE__), "support/**/*.rb")].each {|f| require f}
 
 RSpec.configure do |conf|
   Sinatra::Synchrony.patch_tests!
